570b7d1124437008b8554ac548b164cdd414f180
clz2024.sun <clz2024.sun@gmail.com>
2024-07-01 15:11:07 +0900

    2024-07-01

---
+++ src/com/javaex/ex03/Goods.java
@@ -0,0 +1,13 @@
+package com.javaex.ex03;
+
+//클래스명
+public class Goods {
+
+	//필드 -보호한다(캡슐화)
+	private String name;
+	private int price;
+	
+	//생성자
+	
+	//메소드
+}
---
+++ src/com/javaex/ex03/GoodsApp.java
@@ -0,0 +1,15 @@
+package com.javaex.ex03;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		
+		Goods computer = new Goods();
+		
+		/* 갭슐화 저장할수 없다
+		computer.name = "LG그램";
+		computer.price = 1000000;
+		*/
+	}
+
+}

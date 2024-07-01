185785f77c1e63e9fccf5c0f8a44dbb895a50544
clz2024.sun <clz2024.sun@gmail.com>
2024-07-01 17:18:15 +0900

    2024-07-01

---
+++ src/com/javaex/ex04/Point.java
@@ -0,0 +1,14 @@
+package com.javaex.ex04;
+
+//클래스명
+public class Point {
+
+	//필드
+	
+	//생성자
+	
+	//메소드
+	//get set
+	//일반이름
+	
+}
---
+++ src/com/javaex/ex04/PointApp.java
@@ -0,0 +1,9 @@
+package com.javaex.ex04;
+
+public class PointApp {
+
+	public static void main(String[] args) {
+
+	}
+
+}

f94771964611cdc90ebabcf65518ee022a879158
clz2024.sun <clz2024.sun@gmail.com>
2024-07-01 14:36:48 +0900

    2024-07-01 1st

---
+++ src/com/javaex/ex02/GoodsApp.java
@@ -0,0 +1,36 @@
+package com.javaex.ex02;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		//컴퓨터
+		Goods computer = new Goods();
+		computer.name = "LG그램";
+		computer.price = 1000000;
+		
+		//컵
+		Goods cup = new Goods();
+		cup.name = "머그컵";
+		cup.price = 2000;
+		
+		//니콘추가
+		
+		////////////////////////////////////
+		//컴퓨터가격
+		System.out.println(computer.price);
+		
+		//컵이름
+		
+		//컴퓨터이름
+		
+		//컵가격
+		
+		//컴퓨터가격 --> 필요할때 불러와서 쓸수있다
+		
+		//카메라 이름
+		//카메라 가격
+		
+		
+	}
+
+}

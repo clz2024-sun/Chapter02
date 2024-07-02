650962f5f48d59e6ae5a3e79bc06f8f279e287ca
clz2024.sun <clz2024.sun@gmail.com>
2024-07-02 10:31:37 +0900

    2024-07-02

--- src/com/javaex/ex04/Point.java
+++ src/com/javaex/ex04/Point.java
@@ -3,12 +3,31 @@ package com.javaex.ex04;
 //클래스명
 public class Point {
 
-	//필드
-	
-	//생성자
-	
-	//메소드
-	//get set
-	//일반이름
-	
+	// 필드
+	private int x;
+	private int y;
+
+	// 생성자
+
+	// 메소드
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	// 일반이름
+	public void draw() {
+		System.out.println("점[x="+x+", y="+y+"]을 그렸습니다." );
+	}
 }
--- src/com/javaex/ex04/PointApp.java
+++ src/com/javaex/ex04/PointApp.java
@@ -3,6 +3,17 @@ package com.javaex.ex04;
 public class PointApp {
 
 	public static void main(String[] args) {
+		Point p01 = new Point();
+		Point p02 = new Point();
+
+		p01.setX(2);
+		p01.setY(3);
+
+		p02.setX(100);
+		p02.setY(200);
+
+		p01.draw();
+		p02.draw();
 
 	}
 

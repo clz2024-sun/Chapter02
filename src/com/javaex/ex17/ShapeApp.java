1d42ccf5a6acaf000fc0ecc9473f7f36ff3b4ed0
clz2024.sun <clz2024.sun@gmail.com>
2024-07-09 14:16:48 +0900

    2024-07-09

---
+++ src/com/javaex/ex17/ShapeApp.java
@@ -0,0 +1,35 @@
+package com.javaex.ex17;
+
+public class ShapeApp {
+
+	public static void main(String[] args) {
+		
+		Drawable[] dArray = new Drawable[4];
+		
+		
+		Drawable p01 = new Point(3,6);
+		Drawable r01= new Rectangle(10, 10, "빨강", "검정");
+		Drawable c01 = new Circle(10, "빨강", "검정");
+		Drawable t01 = new Triangle(30, 30, "빨강", "검정");
+		
+		
+		dArray[0] = p01;
+		dArray[1] = r01;
+		dArray[2] = c01;
+		dArray[3] = t01;
+		
+		
+		//dArray[0].draw();
+		//r01.draw();
+		
+		for(int i=0; i<dArray.length; i++) {
+			dArray[i].draw();
+		}
+		
+		
+		
+		
+		
+	}
+
+}
